efbd629e25e210a2bfbffff2793bd381d0610a53
Juan Carlos Coto <jccotou@gmail.com>
2015-08-21 17:15:49 -0600

    Update main.js to use three.js as a parameter to the module

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-(function () {
-    console.log("hello World!");
-})();
+var Main = (function (three) {
+    // Self declaration for holding module public members
+    var self = {};
+
+    self.test_print = function () {
+        console.log("Yes, this is dog");
+    }
+
+    return self;
+})(three);
