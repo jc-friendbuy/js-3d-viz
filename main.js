83d57d0aa2ce150cd2f1400268ad8c159f25b80c
Juan Carlos Coto <jccotou@gmail.com>
2015-08-21 17:46:43 -0600

    Add example code to main.js (test green cube)

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-var Main = (function (three) {
+var Main = (function (THREE) {
     // Self declaration for holding module public members
     var self = {};
 
     self.test_print = function () {
         console.log("Yes, this is dog");
+    }
+
+    self.test_basic_geometry = function() {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        camera.position.z = 5;
+
+        function render() {
+        	requestAnimationFrame( render );
+            cube.rotation.x += 0.1;
+            cube.rotation.y += 0.1;
+        	renderer.render( scene, camera );
+        }
+        render();
     }
 
     return self;
